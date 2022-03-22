06e9d66c6382509720e06a753499f654a07b9640
Radoslaw Moskal <236607@edu.p.lodz.pl>
2022-03-22 14:11:15 +0100

    Program z testami

---
+++ zadanie1/Test1/UnitTest1.cs
@@ -0,0 +1,35 @@
+using NUnit.Framework;
+
+namespace Test1;
+
+public class Tests
+{
+    double x = 3.3;
+    double y = 3.2;
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void TestAdd()
+    {
+        //problem z precyzja po przecinku dlatego jest wykorzystana funkcja within
+        Assert.That(Program.dzialania(1, x, y), Is.EqualTo(6.5).Within(0.0001));
+    }
+    [Test]
+    public void TestSub()
+    {
+        Assert.That(Program.dzialania(2, x, y), Is.EqualTo(0.1).Within(0.0001));
+    }
+    [Test]
+    public void TestDiv()
+    {
+        Assert.That(Program.dzialania(3, x, y), Is.EqualTo(1.03125).Within(0.0001));
+    }
+    [Test]
+    public void TestMul()
+    {
+        Assert.That(Program.dzialania(4, x, y), Is.EqualTo(10.56).Within(0.0001));
+    }
+}
