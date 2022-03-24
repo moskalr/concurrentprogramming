f0e5fcc03accbdc0daf81885eeaaa346418df019
Radoslaw Moskal <236607@edu.p.lodz.pl>
2022-03-24 16:59:18 +0100

    Testy sprawdzone

--- zadanie1/Test1/UnitTest1.cs
+++ zadanie1/Test1/UnitTest1.cs
@@ -6,6 +6,7 @@ public class Tests
 {
     double x = 3.3;
     double y = 3.2;
+    double wynik = 6.5;
     [SetUp]
     public void Setup()
     {
@@ -15,12 +16,12 @@ public class Tests
     public void TestAdd()
     {
         //problem z precyzja po przecinku dlatego jest wykorzystana funkcja within
-        Assert.That(Program.dzialania(1, x, y), Is.EqualTo(6.5).Within(0.0001));
+        Assert.That(Program.dzialania(1,x,y), Is.EqualTo(6.5));
     }
     [Test]
     public void TestSub()
     {
-        Assert.That(Program.dzialania(2, x, y), Is.EqualTo(0.1).Within(0.0001));
+        Assert.That(Program.dzialania(2, x, y), Is.EqualTo(0.1).Within(0.001));
     }
     [Test]
     public void TestDiv()
@@ -30,6 +31,6 @@ public class Tests
     [Test]
     public void TestMul()
     {
-        Assert.That(Program.dzialania(4, x, y), Is.EqualTo(10.56).Within(0.0001));
+        Assert.That(Program.dzialania(4, x, y), Is.EqualTo(10.56));
     }
 }
